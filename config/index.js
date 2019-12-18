82f7456a723ee622cbb3bd9ba3ed1f6d4c9524b3
tkym <lelele5@RyoyanoMacBook-Air.local>
2019-12-18 14:51:06 +0900

    first commit

--- config/index.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  assetsRoot: path.resolve(__dirname, '../dist'),
-  assetsSubDirectory: 'static',
-  assetsPublicPath: ''
-}
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  publicPath: '/tenki',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false
+}
